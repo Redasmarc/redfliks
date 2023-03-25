f303a6ce1b111561c43dfb9325bae4da6b5c3ddb
Redasmarc <marcinkus420@gmail.com>
2023-03-25 19:36:42 +0200

    User registration

--- redflix/user_profile/views.py
+++ redflix/user_profile/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.urls import reverse_lazy
 
-# Create your views here.
+User = get_user_model()
+
+@csrf_protect
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        is_error = False
+        if User.objects.filter(username=username).exists():
+            is_error = True
+            messages.error(request, "Sorry! Username is taken.")
+        if len(username) == 0:
+            is_error = True
+            messages.error(request, "Please enter username.")
+        if len(email) >0 and User.objects.filter(email=email).exists():
+            is_error = True
+            messages.error(request, "Sorry! User with this email address already exists.")
+        if len(email) == 0:
+            is_error = True
+            messages.error(request, "Please enter email address")
+        if password1 != password2:
+            is_error = True
+            messages.error(request, "Passwords do not match.")
+        if len(password1) ==0 or len(password2) == 0:
+            is_error = True
+            messages.error(request, "Please enter a password.")
+        if not is_error:
+            try:
+                User.objects.create_user(username=username, email=email, password=password1)
+            except Exception as e:
+                is_error = True
+                messages.error(request, str(e))
+        if not is_error:
+            messages.success(request, f"Welcome {username}! You have signed up successfully! Please sign in.")
+            return redirect(reverse_lazy('login'))
+
+    return render(request, 'user_profile/register.html')
